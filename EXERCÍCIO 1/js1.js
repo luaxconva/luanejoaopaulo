fa6029ddf44efe594f0256e781ea0204cc45287d
luaxconva <cluane269@gmail.com>
2024-09-11 08:35:43 -0300

    Delete EXERCÍCIO 1 directory

--- EXERCÍCIO 1/js1.js
+++
@@ -1,9 +0,0 @@
-var numero = parseFloat(prompt("'*•.¸♡Insira um número:♡¸.•*'"));
-
-if (numero > 0) {
-    alert(": ̗̀➛O número é positivo!");
-} else if (numero < 0) {
-    alert(": ̗̀➛O número é negativo!");
-} else {
-    alert(": ̗̀➛O número é neutro!");
-}
